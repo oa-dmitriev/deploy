9de95fb4aadc5d26bc119f39bc40c220c90b3bc2
Oleg Dmitriev <dmitrievoleg96@gmail.com>
2020-07-12 02:43:24 +0300

    initial commit

---
+++ simple/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func OilPriceHandler(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().Unix())
+
+	magicNumber := rand.Int()
+
+	w.Write([]byte(strconv.Itoa(magicNumber)))
+}
+
+func main() {
+	http.HandleFunc("/", OilPriceHandler)
+
+	fmt.Println("starting service")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
